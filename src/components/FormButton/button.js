3aad4329e6cd2524f5a2b3d5ce5a6414cc0c4e0c
Danish Sheikh <hetanthakkar@Danishs-Mac-Mini.local>
2020-10-01 12:49:25 +0530

    Edited editoreconfig file

--- src/components/FormButton/button.js
+++ src/components/FormButton/button.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { ActivityIndicator } from 'react-native'
-import { SubmitButtonContainer, SubmitButton, ButtonText } from './styles'
+import React from "react";
+import { ActivityIndicator } from "react-native";
+import { SubmitButtonContainer, SubmitButton, ButtonText } from "./styles";
 
-const flatButton = ({ text, onPress, loading, backgroundColor = '#000' }) => {
-  return (
-    <SubmitButtonContainer onPress={onPress}>
-      <SubmitButton backgroundColor={backgroundColor}>
-        {!loading ? (
-          <ButtonText>{text}</ButtonText>
-        ) : (
-          <ActivityIndicator size="small" />
-        )}
-      </SubmitButton>
-    </SubmitButtonContainer>
-  )
-}
-export default flatButton
+const flatButton = ({ text, onPress, loading, backgroundColor = "#000" }) => {
+    return (
+        <SubmitButtonContainer onPress={onPress}>
+            <SubmitButton backgroundColor={backgroundColor}>
+                {!loading ? (
+                    <ButtonText>{text}</ButtonText>
+                ) : (
+                    <ActivityIndicator size="small" />
+                )}
+            </SubmitButton>
+        </SubmitButtonContainer>
+    );
+};
+export default flatButton;
