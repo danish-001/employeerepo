0b02b827de925be146a407a810ace9951cfe7b68
Danish Sheikh <hetanthakkar@Danishs-Mac-Mini.local>
2020-10-02 17:27:08 +0530

    Added Redux-Persist to the App

--- src/features/home/saga/index.js
+++ src/features/home/saga/index.js
@@ -1,23 +1,22 @@
-import { call, put, takeLatest } from 'redux-saga/effects'
-import Request from '../../../api/request'
-import { GET_EMPLOYEES } from '../constants'
-import { saveEmployees } from '../actions'
+import { call, put, takeLatest } from "redux-saga/effects";
+import Request from "../../../api/request";
+import { GET_EMPLOYEES } from "../constants";
+import { saveEmployees } from "../actions";
 
 function* getEmployees(payloadData) {
-  try {
-    // const options = {
-    //   path: 'employees',
-    // }
-    // const response = yield call([Request, 'get'], options)
-
-    // if (response.data) {
-    //   yield put(saveEmployees({ employees: response.data }))
-    // }
-  } catch (error) {
-    console.log('error', error)
-  }
+    try {
+        const options = {
+            path: "employees",
+        };
+        const response = yield call([Request, "get"], options);
+        if (response.data) {
+            yield put(saveEmployees({ employees: response.data }));
+        }
+    } catch (error) {
+        console.log("error", error);
+    }
 }
 
 export function* homeSaga() {
-  yield takeLatest(GET_EMPLOYEES, getEmployees)
+    yield takeLatest(GET_EMPLOYEES, getEmployees);
 }
