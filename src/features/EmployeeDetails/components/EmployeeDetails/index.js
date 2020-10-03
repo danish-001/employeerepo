8f7eea09697a649958a405e653e141602e48ce07
Danish Sheikh <hetanthakkar@Danishs-Mac-Mini.local>
2020-10-03 10:53:21 +0530

    Added Toast message on Employee Create, Update and Delete

--- src/features/EmployeeDetails/components/EmployeeDetails/index.js
+++ src/features/EmployeeDetails/components/EmployeeDetails/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { ScrollView, KeyboardAvoidingView } from "react-native";
 import { Field, change } from "redux-form";
 import { InputField, FormButton } from "@/components";
-
+import { Toast, Root } from "native-base";
 import {
     EmployeeDetailsContainer,
     FormContainer,
@@ -50,17 +50,56 @@ export class EmployeeDetails extends Component<Props> {
 
     onSubmit = (values) => {
         const { createEmployee, navigation } = this.props;
-        createEmployee({ params: values, navigation });
+        createEmployee({
+            params: values,
+            navigation,
+            onSuccess: () => {
+                Toast.show({
+                    text: "Employee Created Successfully",
+                    duration: 2000,
+                    type: "success",
+                });
+                setTimeout(() => {
+                    navigation.goBack();
+                }, 1500);
+            },
+        });
     };
 
     updateEmployee = (id) => {
         const { updateEmployee, navigation } = this.props;
-        updateEmployee({ params: id, navigation });
+        updateEmployee({
+            params: id,
+            navigation,
+            onSuccess: () => {
+                Toast.show({
+                    text: "Employee Updated",
+                    duration: 2000,
+                    type: "warning",
+                });
+                setTimeout(() => {
+                    navigation.goBack();
+                }, 1500);
+            },
+        });
     };
 
     deleteEmployee = (id) => {
         const { deleteEmployee, navigation } = this.props;
-        deleteEmployee({ params: id, navigation });
+        deleteEmployee({
+            params: id,
+            navigation,
+            onSuccess: () => {
+                Toast.show({
+                    text: "Employee Deleted Successfully",
+                    duration: 2000,
+                    type: "danger",
+                });
+                setTimeout(() => {
+                    navigation.goBack();
+                }, 1500);
+            },
+        });
     };
 
     render() {
@@ -74,81 +113,90 @@ export class EmployeeDetails extends Component<Props> {
 
         const empRef = {};
         return (
-            <EmployeeDetailsContainer>
-                <ScrollView keyboardShouldPersistTaps="handled">
-                    <KeyboardAvoidingView>
-                        <FormContainer>
-                            <EmployeeName>Employee Name</EmployeeName>
-                            <TextInputField>
-                                <Field
-                                    refLink={(ref) => (empRef.name = ref)}
-                                    name="name"
-                                    component={InputField}
-                                    placeholder="Name"
-                                    autoFocus={true}
-                                    onSubmitEditing={() => {
-                                        empRef.salary.focus();
-                                    }}
-                                />
-                            </TextInputField>
-                            <EmployeeSalary>Employee Salary</EmployeeSalary>
-                            <TextInputField>
-                                <Field
-                                    refLink={(ref) => (empRef.salary = ref)}
-                                    name="salary"
-                                    component={InputField}
-                                    placeholder="Salary (in Rupees)"
-                                    keyboardType="numeric"
-                                    onSubmitEditing={() => {
-                                        empRef.age.focus();
-                                    }}
-                                />
-                            </TextInputField>
-                            <EmployeeAge>Employee Age</EmployeeAge>
-                            <TextInputField>
-                                <Field
-                                    name="age"
-                                    refLink={(ref) => (empRef.age = ref)}
-                                    component={InputField}
-                                    placeholder="Age"
-                                    keyboardType="numeric"
-                                />
-                            </TextInputField>
-                            <ButtonContainer>
-                                {route.params && route.params.defaultValues ? (
-                                    <DualButtonContainer>
-                                        <UpdateButtonContainer>
-                                            <FormButton
-                                                text="Update"
-                                                onPress={handleSubmit(
-                                                    this.updateEmployee
-                                                )}
-                                                loading={updateEmployeeLoading}
-                                            />
-                                        </UpdateButtonContainer>
-                                        <DeleteButtonContainer>
-                                            <FormButton
-                                                text="Delete"
-                                                onPress={handleSubmit(
-                                                    this.deleteEmployee
-                                                )}
-                                                loading={deleteEmployeeLoading}
-                                                backgroundColor="orangered"
-                                            />
-                                        </DeleteButtonContainer>
-                                    </DualButtonContainer>
-                                ) : (
-                                    <FormButton
-                                        text="Submit"
-                                        onPress={handleSubmit(this.onSubmit)}
-                                        loading={createEmployeeLoading}
+            <Root>
+                <EmployeeDetailsContainer>
+                    <ScrollView keyboardShouldPersistTaps="handled">
+                        <KeyboardAvoidingView>
+                            <FormContainer>
+                                <EmployeeName>Employee Name</EmployeeName>
+                                <TextInputField>
+                                    <Field
+                                        refLink={(ref) => (empRef.name = ref)}
+                                        name="name"
+                                        component={InputField}
+                                        placeholder="Name"
+                                        autoFocus={true}
+                                        onSubmitEditing={() => {
+                                            empRef.salary.focus();
+                                        }}
                                     />
-                                )}
-                            </ButtonContainer>
-                        </FormContainer>
-                    </KeyboardAvoidingView>
-                </ScrollView>
-            </EmployeeDetailsContainer>
+                                </TextInputField>
+                                <EmployeeSalary>Employee Salary</EmployeeSalary>
+                                <TextInputField>
+                                    <Field
+                                        refLink={(ref) => (empRef.salary = ref)}
+                                        name="salary"
+                                        component={InputField}
+                                        placeholder="Salary (in Rupees)"
+                                        keyboardType="numeric"
+                                        onSubmitEditing={() => {
+                                            empRef.age.focus();
+                                        }}
+                                    />
+                                </TextInputField>
+                                <EmployeeAge>Employee Age</EmployeeAge>
+                                <TextInputField>
+                                    <Field
+                                        name="age"
+                                        refLink={(ref) => (empRef.age = ref)}
+                                        component={InputField}
+                                        placeholder="Age"
+                                        keyboardType="numeric"
+                                    />
+                                </TextInputField>
+                                <ButtonContainer>
+                                    {route.params &&
+                                    route.params.defaultValues ? (
+                                        <DualButtonContainer>
+                                            <UpdateButtonContainer>
+                                                <FormButton
+                                                    text="Update"
+                                                    onPress={handleSubmit(
+                                                        this.updateEmployee
+                                                    )}
+                                                    loading={
+                                                        updateEmployeeLoading
+                                                    }
+                                                />
+                                            </UpdateButtonContainer>
+                                            <DeleteButtonContainer>
+                                                <FormButton
+                                                    text="Delete"
+                                                    onPress={handleSubmit(
+                                                        this.deleteEmployee
+                                                    )}
+                                                    loading={
+                                                        deleteEmployeeLoading
+                                                    }
+                                                    backgroundColor="orangered"
+                                                />
+                                            </DeleteButtonContainer>
+                                        </DualButtonContainer>
+                                    ) : (
+                                        <FormButton
+                                            text="Submit"
+                                            onPress={handleSubmit(
+                                                this.onSubmit
+                                            )}
+                                            loading={createEmployeeLoading}
+                                        />
+                                    )}
+                                </ButtonContainer>
+                            </FormContainer>
+                        </KeyboardAvoidingView>
+                    </ScrollView>
+                </EmployeeDetailsContainer>
+            </Root>
         );
     }
 }
--- src/features/EmployeeDetails/saga/index.js
+++ src/features/EmployeeDetails/saga/index.js
@@ -1,82 +1,84 @@
-import { call, put, takeLatest, takeEvery } from 'redux-saga/effects'
-import { CREATE_EMPLOYEE, DELETE_EMPLOYEE, UPDATE_EMPLOYEE } from '../constants'
-
-import Request from '@/api/request'
+import { call, put, takeLatest, takeEvery } from "redux-saga/effects";
 import {
-  spinner,
-  saveEmployee,
-  setRemovedEmployee,
-  setUpdateEmployee,
-} from '../actions'
+    CREATE_EMPLOYEE,
+    DELETE_EMPLOYEE,
+    UPDATE_EMPLOYEE,
+} from "../constants";
+import Request from "@/api/request";
+import {
+    spinner,
+    saveEmployee,
+    setRemovedEmployee,
+    setUpdateEmployee,
+} from "../actions";
 
 export function* createEmployee(payloadData) {
-  try {
-    yield put(spinner({ createEmployeeLoading: true }))
+    try {
+        yield put(spinner({ createEmployeeLoading: true }));
 
-    const options = {
-      path: 'create',
-      body: payloadData.payload.params,
+        const options = {
+            path: "create",
+            body: payloadData.payload.params,
+        };
+
+        const response = yield call([Request, "post"], options);
+
+        if (response.status === "success") {
+            yield put(saveEmployee({ employee: payloadData.payload.params }));
+
+            payloadData.payload.onSuccess();
+        }
+    } catch (error) {
+        console.log("error", error);
+    } finally {
+        yield put(spinner({ createEmployeeLoading: false }));
     }
-
-    const response = yield call([Request, 'post'], options)
-
-    if (response.status === 'success') {
-      yield put(saveEmployee({ employee: payloadData.payload.params }))
-
-      payloadData.payload.navigation.goBack()
-    }
-  } catch (error) {
-    console.log('error', error)
-  } finally {
-    yield put(spinner({ createEmployeeLoading: false }))
-  }
 }
 
 export function* deleteEmployeeAt(payloadData) {
-  try {
-    yield put(spinner({ deleteEmployeeLoading: true }))
+    try {
+        yield put(spinner({ deleteEmployeeLoading: true }));
 
-    const options = {
-      path: `delete/${payloadData.payload.params.id}`,
+        const options = {
+            path: `delete/${payloadData.payload.params.id}`,
+        };
+
+        const response = yield call([Request, "delete"], options);
+
+        if ((response.status = "success")) {
+            yield put(setRemovedEmployee({ employee: payloadData.payload }));
+            payloadData.payload.onSuccess();
+        }
+    } catch (error) {
+        console.log("error", error);
+    } finally {
+        yield put(spinner({ deleteEmployeeLoading: false }));
     }
-
-    const response = yield call([Request, 'delete'], options)
-
-    if ((response.status = 'success')) {
-      yield put(setRemovedEmployee({ employee: payloadData.payload }))
-
-      payloadData.payload.navigation.goBack()
-    }
-  } catch (error) {
-    console.log('error', error)
-  } finally {
-    yield put(spinner({ deleteEmployeeLoading: false }))
-  }
 }
 
 export function* updateEmployeeAt(payloadData) {
-  try {
-    yield put(spinner({ updateEmployeeLoading: true }))
-    const options = {
-      path: `update/${payloadData.payload.params.id}`,
-      body: payloadData.payload.params,
-    }
+    try {
+        yield put(spinner({ updateEmployeeLoading: true }));
+        const options = {
+            path: `update/${payloadData.payload.params.id}`,
+            body: payloadData.payload.params,
+        };
 
-    const response = yield call([Request, 'put'], options)
+        const response = yield call([Request, "put"], options);
 
-    if (response.status === 'success') {
-      yield put(setUpdateEmployee({ employee: response.data }))
-      payloadData.payload.navigation.goBack()
+        if (response.status === "success") {
+            yield put(setUpdateEmployee({ employee: response.data }));
+            payloadData.payload.onSuccess();
+        }
+    } catch (error) {
+        console.log("error", error);
+    } finally {
+        yield put(spinner({ updateEmployeeLoading: false }));
     }
-  } catch (error) {
-    console.log('error', error)
-  } finally {
-    yield put(spinner({ updateEmployeeLoading: false }))
-  }
 }
 
 export function* employeeSaga() {
-  yield takeLatest(CREATE_EMPLOYEE, createEmployee)
-  yield takeLatest(DELETE_EMPLOYEE, deleteEmployeeAt)
-  yield takeEvery(UPDATE_EMPLOYEE, updateEmployeeAt)
+    yield takeLatest(CREATE_EMPLOYEE, createEmployee);
+    yield takeLatest(DELETE_EMPLOYEE, deleteEmployeeAt);
+    yield takeEvery(UPDATE_EMPLOYEE, updateEmployeeAt);
 }
